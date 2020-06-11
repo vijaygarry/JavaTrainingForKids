12d5a4b552dd2640041243243f2742d936eabcea
Vijay Garothaya <vijay_garry@hotmail.com>
2020-06-10 23:33:51 -0400

    Sample to read input.

--- projects/ReadConsole/src/ReadExample.java
+++ projects/ReadConsole/src/ReadExample.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 public class ReadExample {
 	public static void main(String[] args) {
 		
-		System.out.print( "Enter your name: ");
-		String name = readUserStringInput();
-		System.out.print( "Enter your last name: ");
-		String lastName = readUserStringInput();
 		
-		
+		String name = readUserStringInput("Enter your name:");
+		String lastName = readUserStringInput("Enter your last name:");
 		System.out.print( "Your full name: " + name + " " + lastName + " :)");
-		System.out.print( "Enter num1: ");
-		int num1 = readUserIntInput();
-		System.out.print( "Enter num2: ");
-		int num2 = readUserIntInput();
+		
+		
+		int num1 = readUserIntInput("Enter num1:");
+		int num2 = readUserIntInput("Enter num2:");
 		int sum = num1 + num2;
 		System.out.println( "Sum " + sum );
 	}
 	
-	public static String readUserStringInput () {
+	
+	public static String readUserStringInput (String aInputMsg) {
+		System.out.print( aInputMsg + " ");
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next();
 	}
 	
-	public static int readUserIntInput () {
+	
+	public static int readUserIntInput (String aInputMsg) {
+		System.out.print( aInputMsg + " ");
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextInt();
 	}
